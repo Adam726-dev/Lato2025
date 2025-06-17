b430c4f012ea5aadf162934e9ce16d0645f6d45e
Adam726-dev <>
2025-06-17 19:26:43 +0200

    dodany service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('../public/sw.js').then(
+      (registration) => {
+        console.log('Service Worker zarejestrowany:', registration);
+      },
+      (error) => {
+        console.error('Błąd rejestracji Service Workera:', error);
+      }
+    );
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
